3b25842f7c0c144c6a36e3a04f86f0683e2e7a29
adii1203 <aditya32ft@gmail.com>
2025-06-23 21:12:13 +0530

    db migrations

---
+++ internal/db/migrations/000005_create_user_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email TEXT NOT NULL UNIQUE,
+    first_name TEXT NOT NULL,
+    last_name TEXT
+);
--- internal/db/migrations/20250611101523_create_projects.up.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE projects (
-    id UUID PRIMARY KEY,
-    name TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT now()
-);
